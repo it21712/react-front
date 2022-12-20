9de6b8f7dc664c211c893283b39441af6bcf9d85
ntinos <ntinos@deanz.ntinos>
2022-12-20 19:13:11 +0200

    genesis

---
+++ src/components/RoleButton.js
@@ -0,0 +1,15 @@
+const RoleButton = ({ icon, text }) => {
+    return (
+        <div className="flex flex-col w-40 h-40 bg-gray-300 rounded-xl items-center shadow-xl shadow-slate-400
+        transition ease-in-out duration-300 hover:-translate-y-5 hover:bg-gray-600 cursor-pointer group">
+            <div className="flex flex-col m-auto items-center space-y-4">
+                {icon}
+                <h4 className=" text-gray-500 font-bold transition ease-in-out duration-300 group-hover:text-white">{text}</h4>
+            </div>
+
+        </div>
+    );
+}
+
+
+export default RoleButton;
---
+++ src/strings.js
@@ -0,0 +1,5 @@
+export const appTitle = 'ΧΑΡΟΚΟΠΕΙΟ ΠΑΝΕΠΙΣΤΗΜΙΟ - ΣΥΣΤΗΜΑ ΠΡΟΓΡΑΜΜΑΤΩΝ ΕΛΚΕ';
+export const welcomeText = 'Καλως Ορίσατε στην πλατφόρμα προγραμμάτων ΕΛΚΕ του Χαροκόπειου Πανεπιστημίου';
+export const appLoginMethodText = 'Επιλέξτε πως θέλετε να συνδεθείτε:';
+export const evaluatorText = 'Αξιολογητής (Σύνδεση με SSO)';
+export const applicantText = 'Αιτών';
