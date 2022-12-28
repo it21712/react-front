b19b60c0af77b4d415564df2541099063013db25
Konstantinos Vrs <it21712@hua.gr>
2022-12-28 14:59:38 +0200

    applicant profile page v1

--- src/screens/ApplicantPage.js
+++ src/screens/ApplicantPage.js
@@ -1,4 +1,4 @@
-import { accountDetails, appTitle, contactText, invitationsText, logoutText } from "../strings";
+import { accountDetails, addressText, appTitle, cityText, contactText, countryText, dateOfBirthText, firstNameText, invitationsText, lastNameText, logoutText } from "../strings";
 import useAuth from "../hooks/useAuth";
 import useAxiosPrivate, { useAxiosRole } from "../hooks/useAxiosPrivate";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -35,18 +35,14 @@ const ApplicantPage = () => {
 
     return (
         <div className="flex flex-col h-screen">
-            <div className="flex bg-gray-800 shadow-md fixed w-full z-10 top-0 h-16 px-3 justify-start items-center">
-                
-                
+            <div className="flex bg-gray-800 shadow-md fixed w-full z-10 top-0 h-16 px-3 justify-start items-center">               
                 <h3 className='font-normal invisible sm:visible text-white my-auto'>{appTitle}</h3>
-                
             </div>
+
             <div className='flex h-full w-screen'>
                 <SidebarDrawer email={auth?.email}/>
                 <UserDetailsFragment/>
-            </div>
-            
-            
+            </div>          
 
             {logout && <Navigate to={homeRoute} replace />}
         </div>
@@ -72,8 +68,8 @@ const SidebarDrawer = ({email}) => {
     return (
         <div className='flex h-screen w-[23%]'>
                 {/* <div className='flex flex-col transition -translate-x-[80%] ease-in duration:700 w-[23%] hover:translate-x-0 bg-gray-800 mt-16 items-center px-6'>            */}
-                <div className='flex flex-col bg-gray-800 mt-16 px-6 w-full'>
-                    <div className='mt-6 mx-auto'>
+                <div className='flex flex-col bg-gray-800 mt-16 px-6 w-full h-full'>
+                    <div className='mt-6 mx-auto w-12 h-12'>
                         <ProfileAvatar/>
                     </div>
                     <div className='border-b-[1px] border-white w-full mx-auto mb-10'>
@@ -93,7 +89,7 @@ const SidebarDrawer = ({email}) => {
 
 const ProfileAvatar = ({pic}) => {
     return (
-        <div className='flex w-14 h-14 items-center justify-center'>
+        <div className='flex w-full h-full items-center justify-center '>
             <img src='/unknown_avatar.png' alt='unknown' className='object-cover rounded-full'></img>
         </div>
     );
@@ -110,8 +106,51 @@ const SidebarAction = ({content, handleClick, icon}) => {
 
 const UserDetailsFragment = () => {
     return (
-        <div className='bg-gray-200 w-[77%] h-full'>
+        <div className='flex bg-gray-200 w-[77%] h-full mt-16 justify-center'>
+            <form className='mt-6 flex flex-col'>
+                <div className='flex items-center mt-6 mb-12'>
+                    <div className='w-24 h-24'>
+                        <ProfileAvatar/>
+                    </div>
+                    
+                    <input type='file' id='profilePic' className='py-2 pl-4 border-2 rounded-sm text-gray-700 leading-tight focus:outline-none '/>
+                </div>
 
+                <div className='flex'>
+                    
+                    <div className='flex flex-col justify-start items-start pr-6'>
+                        <h2>{firstNameText}</h2>
+                        <input type='text' id='firstName' className='w-full py-2 px-3 border-2 rounded-sm text-gray-700 leading-tight mt-2 focus:outline-none '></input>
+                    </div>
+                    <div className='flex flex-col justify-start items-start'>
+                        <h2>{lastNameText}</h2>
+                        <input type='text' id='lastName' className='w-full py-2 px-3 border-2 rounded-sm text-gray-700 leading-tight mt-2 focus:outline-none '></input>
+                    </div>
+                </div>
+
+                <div className='flex flex-col items-start mt-6'>
+                    <h2>Email</h2>
+                    <input type='email' id='email' className='w-full py-2 px-3 border-2 rounded-sm text-gray-700 leading-tight mt-2 focus:outline-none '/>
+                </div>
+
+                <div className='flex flex-col items-start mt-6'>
+                    <h2>{dateOfBirthText}</h2>
+                    <input type='date' id='date' className='w-full py-2 px-3 border-2 rounded-sm text-gray-700 leading-tight mt-2 focus:outline-none '/>
+                </div>
+
+                <div className='flex flex-col items-start mt-6'>
+                <h2>{countryText}</h2>
+                    <input type='text' id='country' className='w-full py-2 px-3 border-2 rounded-sm text-gray-700 leading-tight mt-2 focus:outline-none '/>
+
+                    <h2 className='mt-3'>{cityText}</h2>
+                    <input type='text' id='city' className='w-full py-2 px-3 border-2 rounded-sm text-gray-700 leading-tight mt-2 focus:outline-none '/>
+                
+                    <h1 className='text-lg mb-3'>{addressText}</h1>
+                    
+                </div>
+
+                
+            </form>
         </div>
     );
 }
